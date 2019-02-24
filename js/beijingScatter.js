20890881a3549e0f79c8f072e25290c26637a171
zhengshuyu1996 <zhengshuyu1996@163.com>
2019-02-24 15:09:10 +0800

    调整图表大小

--- js/beijingScatter.js
+++ js/beijingScatter.js
@@ -3,7 +3,7 @@ var beijingScatterContainer = document.getElementById('beijing-scatter');
 //用于使chart自适应高度和宽度,通过窗体高宽计算容器高宽
 function resizeBeijingScatterContainer () {
     beijingScatterContainer.style.width = (window.innerWidth * 0.4 - 20) + 'px';
-    beijingScatterContainer.style.height = (window.innerHeight * 0.3 - 14) + 'px';
+    beijingScatterContainer.style.height = (window.innerHeight * 0.3 - 12) + 'px';
 }
 //设置容器高宽
 resizeBeijingScatterContainer();
